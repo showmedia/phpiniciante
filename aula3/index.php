82ec958ea30128e712d6f7c807e66820ef1c4f8d
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-06-29 16:01:16 -0300

    adicionando aula 03

---
+++ aula3/index.php
@@ -0,0 +1,34 @@
+<?php 
+
+//criando uma variável e adicionando o valor a ela;
+$nota = 7;
+$media = 6;
+
+
+//estrutura condicional
+// > MAIOR
+// < MENOR
+// == IGUAL
+// <= MENOR OU IGUAL
+// >= MAIOR OU IGUAL
+
+if($nota > $media /* && $nota < $media */){
+    echo 'Aprovado';
+}else if($nota == $media){
+    echo 'Na Média';
+}else{
+    echo 'Repovado';
+}
+
+?>
+<br><br>
+<?php 
+
+//estrutura de repetição
+
+for($i=0;$i<10;$i++){
+    echo 'Pedro Neres - '. $i . '<br>';
+}
+
+echo 'acabou';
+?>
