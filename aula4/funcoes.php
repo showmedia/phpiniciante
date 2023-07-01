3c892e02cd10c3e795cc1e19846e59e0f40be3f3
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-07-01 11:57:18 -0300

    adicionando a aula 04

---
+++ aula4/funcoes.php
@@ -0,0 +1,14 @@
+<?php 
+
+//function para somar dois parametros
+function somar($numerador=0, $denominador=0){
+    $soma = $numerador + $denominador;
+    return $soma;
+}
+//funciton para multiplicar dois parametros
+function multiplicar($num, $den){
+    $result = $num*$den;
+    return $result;
+}
+
+?>
---
+++ aula4/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+require 'funcoes.php';
+
+$den = 10;
+$num = 12;
+echo 'A soma é '.somar($num,$den);
+?>
+
+<br>
+
+<?php 
+echo 'O valor da multiplicação é '.multiplicar($num,$den);
+?>
